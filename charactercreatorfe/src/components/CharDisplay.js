727e9325df950756b2567a1c69127d7833f66b97
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2022-12-24 05:55:09 -0500

    birthday added

--- charactercreatorfe/src/components/CharDisplay.js
+++ charactercreatorfe/src/components/CharDisplay.js
@@ -1,8 +1,49 @@
 
-function CharDisplay({ Character }) {
+function CharDisplay({ character, deleteCharacter }) {
 
     return (
-        <div>
+        <div className="CharacterBlock">
+            
+            <h1> {character.first_Name}</h1>
+            <br></br>
+            <span>Last Name: </span> {character.last_Name}
+            <br></br>
+            <span>Age: </span> {character.age}
+            <br></br>  
+            <span>Birthday: </span> {character.Birthday}
+            <br></br>
+            <span>Nickname: </span>{character.nickname}
+            <br></br>
+            <span>Gender: </span>{character.gender}
+            <br></br>
+            <span>Personality: </span>{character.primaryPersonalityTrait}
+            <br></br>
+            <span>Accessories: </span>{character.primaryAccessory}
+            <br></br>
+            <span>Height: </span>{character.height}
+            <br></br>
+            <span>Weight: </span>{character.weight}
+            <br></br>
+            <span>Eye color: </span>{character.eyeColor}
+            <br></br>
+            <span>Hair Color: </span>{character.hairColor}
+            <br></br>
+            <span>Skin Color: </span>{character.skinColor}
+            <br></br>
+            <span>Residence: </span>{character.residence}
+            <br></br>
+            <span>Ethnicity: </span>{character.ethnicity}
+            <br></br>
+            <span>Occupation: </span>{character.occupation}
+            
+            <button className="delete" onClick={() => { deleteCharacter(character.id) }}>delete Character</button>
+
+        </div>
+    )
+}
+
+/*
+<li>
             {Character.firstName}
             <br></br>
             {Character.lastName}
@@ -32,9 +73,7 @@ function CharDisplay({ Character }) {
             {Character.ethnicity}
             <br></br>
             {Character.occupation}
-        </div>
-    )
-}
-
+            </li>
+*/
 
 export default CharDisplay;
--- charactercreatorfe/src/components/CharList.js
+++ charactercreatorfe/src/components/CharList.js
@@ -1,122 +1,142 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios"; 
+import axios from "axios";
 import { useEffect, useState } from 'react';
 import CharDisplay from './CharDisplay';
-
+import { ToastContainer, toast } from 'react-toastify';
 
 
 function CharList() {
- 
+
     const [character, setCharacters] = useState([]);
 
     const handleSubmit = (e) => {
 
         e.preventDefault();
-        const firstNameInput= e.target.elements.firstName.value;
-        const lastNameInput= e.target.elements.last_Name.value;
-        const NickNameInput= e.target.elements.NickName.value;
-        const ageInput= e.target.elements.age.value; 
-        const genderInput= e.target.elements.Gender.value;
-        const primaryPersonalityTraitInput= e.target.elements.primaryPersonalityTrait.value;
-        const primaryAccessoryInput= e.target.elements.primaryAccessory.value; 
-        const heightInput= e.target.elements.height.value;
-        const weightInput= e.target.elements.weight.value; 
-        const eyeColorInput= e.target.elements.eyeColor.value;
-        const hairColorInput= e.target.elements.hairColor.value; 
-        const skinColorInput= e.target.elements.skinColor.value;
-        const residenceInput= e.target.elements.residence.value;
-        const ethnicityInput= e.target.elements.ethnicity.value;
-        const occupationInput= e.target.elements.occupation.value; 
-        
-        addCharacter({ 
-            firstName : firstNameInput, 
-            last_Name : lastNameInput,
-            NickName : NickNameInput,
-            age : ageInput,
-            Gender : genderInput,
-            primaryPersonalityTrait : primaryPersonalityTraitInput,
-            primaryAccessory : primaryAccessoryInput,
-            height : heightInput,
-            weight : weightInput,
-            eyeColor : eyeColorInput,
-            hairColor : hairColorInput,
-            skinColor : skinColorInput,
-            residence : residenceInput, 
-            ethnicity : ethnicityInput,
-            occupation : occupationInput
+        const firstNameInput = e.target.elements.firstName.value;
+        const lastNameInput = e.target.elements.last_Name.value;
+        const NickNameInput = e.target.elements.NickName.value;
+        const ageInput = e.target.elements.age.value;
+        const birthdayInput = e.target.elements.Birthday.value; 
+        const genderInput = e.target.elements.Gender.value;
+        const primaryPersonalityTraitInput = e.target.elements.primaryPersonalityTrait.value;
+        const primaryAccessoryInput = e.target.elements.primaryAccessory.value;
+        const heightInput = e.target.elements.height.value;
+        const weightInput = e.target.elements.weight.value;
+        const eyeColorInput = e.target.elements.eyeColor.value;
+        const hairColorInput = e.target.elements.hairColor.value;
+        const skinColorInput = e.target.elements.skinColor.value;
+        const residenceInput = e.target.elements.residence.value;
+        const ethnicityInput = e.target.elements.ethnicity.value;
+        const occupationInput = e.target.elements.occupation.value;
+
+        addCharacter({
+            firstName: firstNameInput,
+            last_Name: lastNameInput,
+            NickName: NickNameInput,
+            age: ageInput,
+            Birthday: birthdayInput,
+            Gender: genderInput,
+            primaryPersonalityTrait: primaryPersonalityTraitInput,
+            primaryAccessory: primaryAccessoryInput,
+            height: heightInput,
+            weight: weightInput,
+            eyeColor: eyeColorInput,
+            hairColor: hairColorInput,
+            skinColor: skinColorInput,
+            residence: residenceInput,
+            ethnicity: ethnicityInput,
+            occupation: occupationInput
         })
-    
-      }
+
+    }
 
     const loadCharactersFromAPI = () => {
         axios.get("http://localhost:8081/api/characters")
-        .then(function (response) {
-            const allItems = response.data;
-            setCharacters(allItems);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
+            .then(function (response) {
+                const allItems = response.data;
+                setCharacters(allItems);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
-      const addCharacter = (character) => {
+    const addCharacter = (character) => {
         axios.post("http://localhost:8081/api/characters", character)
-        .then(function (response) {
-            loadCharactersFromAPI();
-        })
-        .catch(function (error){
-            console.log(error); 
-        })
-      }
+            .then(function (response) {
+                loadCharactersFromAPI();
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
-      useEffect(() => { loadCharactersFromAPI(); }, [])
+    useEffect(() => { loadCharactersFromAPI(); }, [])
 
-      const deleteCharacter = (id) => {
+    const deleteCharacter = (id) => {
         axios.delete("http://localhost:8081/api/characters/" + id)
-        .then(function (response) {
-            loadCharactersFromAPI();
-        })
-        .catch(function (error) {
-        console.log(error);
-      })
-      }
+            .then(function (response) {
+                loadCharactersFromAPI();
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
- /*  
+    /*  
+   
+       */
 
-    */
 
     return (
         <div class="container">
             <div class="row" >
-                <h1 className='Header'>Character Creator</h1>
+            <div class="col-sm"> 
+                <h2 className='Header'>Character Creator</h2>
                 <hr></hr>
+            </div> 
+            
             </div>
             <div class="row">
                 <div class="col-sm">
                     <h1>My characters</h1>
-                    
+                    <ul>
+                        {
+                            character.map((character) => (
+                                <li key={character.id}>
+                                    <CharDisplay character={character}
+                                    deleteCharacter = {deleteCharacter}
+                                    >
+
+                                    </CharDisplay>
+                                </li>
+                            ))
+
+                        }
+                    </ul>
                 </div>
                 <div class="col-sm">
                     <h1>Create a new Character!</h1>
                     <form onSubmit={handleSubmit}>
-                     <input type="text" required minLenght={1} name='firstName' placeholder="Enter first name..." />
-                     <input type="text" name='last_Name' placeholder="Enter last name..." />
-                     <input type="text" name='age' placeholder="Enter age..." />
-                     <input type="text" name='NickName' placeholder="Enter nickname..." />
-                     <input type="text" name='Gender' placeholder="Enter gender..." />
-                     <input type="text" name='primaryPersonalityTrait' placeholder="Enter primaryPersonalityTrait..." />
-                     <input type="text" name='primaryAccessory' placeholder="Enter primaryAccessory..." />
-                     <input type="text" name='height' placeholder="Enter height..." />
-                     <input type="text" name='weight' placeholder="Enter weight..." />
-                     <input type="text" name='eyeColor' placeholder="Enter eyeColor..." />
-                     <input type="text" name='hairColor' placeholder="Enter hairColor..." />
-                     <input type="text" name='skinColor' placeholder="Enter skinColor..." />
-                     <input type="text" name='residence' placeholder="Enter residence..." />
-                     <input type="text" name='ethnicity' placeholder="Enter ethnicity..." />
-                     <input type="text" name='occupation' placeholder="Enter occupation..." />
+                        <input type="text" required minLenght={1} name='firstName' placeholder="Enter first name..." />
+                        <input type="text" name='last_Name' placeholder="Enter last name..." />
+                        <input type="text" name='age' placeholder="Enter age..." />
+                        <input type="text" name='Birthday' placeholder="Enter birthday..." />
+                        <input type="text" name='NickName' placeholder="Enter nickname..." />
+                        <input type="text" name='Gender' placeholder="Enter gender..." />
+                        <input type="text" name='primaryPersonalityTrait' placeholder="Enter primaryPersonalityTrait..." />
+                        <input type="text" name='primaryAccessory' placeholder="Enter primaryAccessory..." />
+                        <input type="text" name='height' placeholder="Enter height..." />
+                        <input type="text" name='weight' placeholder="Enter weight..." />
+                        <input type="text" name='eyeColor' placeholder="Enter eyeColor..." />
+                        <input type="text" name='hairColor' placeholder="Enter hairColor..." />
+                        <input type="text" name='skinColor' placeholder="Enter skinColor..." />
+                        <input type="text" name='residence' placeholder="Enter residence..." />
+                        <input type="text" name='ethnicity' placeholder="Enter ethnicity..." />
+                        <input type="text" name='occupation' placeholder="Enter occupation..." />
 
-                     <button>Submit</button>
-            </form>
+                        <button>Create</button>
+                    </form>
                 </div>
             </div>
         </div>
