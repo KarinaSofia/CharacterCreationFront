63e656f34440bdef8caad6456ada5beb478d30b1
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2022-12-24 01:07:12 -0500

    load api, add characters and event handler

---
+++ charactercreatorfe/src/components/CharList.js
@@ -0,0 +1,76 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios"; 
+
+
+function CharList() {
+ 
+    const [character, setCharacters] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const firstNameInput= e.target.elements.firstName.value;
+        const lastNameInput= e.target.elements.lastName.value;
+        const nickNameInput= e.target.elements.nickname.value;
+        const ageInput= e.target.elements.age.value; 
+        const genderInput= e.target.elements.gender.value;
+        const primaryPersonalityTraitInput= e.target.elements.primaryPersonalityTrait.value;
+        const primaryAccessoryInput= e.target.elements.primaryAccessory.value; 
+        const heightInput= e.target.elements.height.value;
+        const weightInput= e.target.elements.weight.value; 
+        const eyeColorInput= e.target.elements.eyeColor.value;
+        const hairColorInput= e.target.elements.hairColor.value; 
+        const skinColorInput= e.target.elements.skinColor.value;
+        const residenceInput= e.target.elements.residence.value;
+        const ethnicityInput= e.target.elements.ethnicity.value;
+        const occupationInput= e.target.elements.occupation.value; 
+        
+        addItem({ title : input1, description : input2})
+    
+      }
+
+    const loadCharactersFromAPI = () => {
+        axios.get("http://localhost:8081/api/characters")
+        .then(function (response) {
+            const allItems = response.data;
+            setTodos(allItems);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+
+      const addCharacter = (character) => {
+        axios.post("http://localhost:8081/api/characters", character)
+        .then(function (response) {
+            loadCharactersFromAPI();
+        })
+        .catch(function (error){
+            console.log(error); 
+        })
+      }
+
+      useEffect(() => { loadCharactersFromAPI(); }, [])
+ /*  
+   
+    */
+
+    return (
+        <div class="container">
+            <div class="row" >
+                <h1 className='Header'>Character Creator</h1>
+                <hr></hr>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <h1>character list</h1>
+                </div>
+                <div class="col-sm">
+                    <h1>submit</h1>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default CharList;
